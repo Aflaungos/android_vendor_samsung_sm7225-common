82b950ad906764c75497149b7818b826d39122ef
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-14 12:15:19 -0300

    sm7225-common-vendor.mk: Add gnss QTI service as product

--- sm7225-common/sm7225-common-vendor.mk
+++ sm7225-common/sm7225-common-vendor.mk
@@ -1036,6 +1036,7 @@ PRODUCT_PACKAGES += \
     libskeymaster4device \
     CACertService \
     TimeService \
+    android.hardware.gnss@2.1-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti-hta \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
