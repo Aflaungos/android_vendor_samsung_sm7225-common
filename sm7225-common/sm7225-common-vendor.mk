cf740d77f335195b8c7e4039009aa807bcba5495
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-14 22:00:17 -0300

    sm7225-common-vendor.mk: Remove Lights

--- sm7225-common/sm7225-common-vendor.mk
+++ sm7225-common/sm7225-common-vendor.mk
@@ -1045,6 +1045,5 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3-service-qti-hta \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
-    manifest_lights-default-sec \
     com.qualcomm.qti.gpudrivers.lito.api30 \
     manifest_vendor.qti.hardware.display.composer-service
