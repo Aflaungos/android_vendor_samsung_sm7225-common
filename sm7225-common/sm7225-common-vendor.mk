d8449f30ebcf853ba1f695849f8e7b40faf3b0d1
Aflaungos <vimavick@gmail.com>
2024-04-22 15:51:04 -0300

    Revert "sm7225-common-vendor.mk: Remove GNSS QTI service"
    This reverts commit ba721d5f5f98b17f371c47b399bb955786fbe172.

--- sm7225-common/sm7225-common-vendor.mk
+++ sm7225-common/sm7225-common-vendor.mk
@@ -1054,6 +1054,7 @@ PRODUCT_PACKAGES += \
     libskeymaster4device \
     CACertService \
     TimeService \
+    android.hardware.gnss@2.1-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti-hta \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
